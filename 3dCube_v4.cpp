d2c0caf445377bfafad0950ae6d723bf56ba7d55
John Eriksson <john5@kth.se>
2020-01-21 18:59:32 +0100

    Working on it

--- 3dCube_v4.cpp
+++ 3dCube_v4.cpp
@@ -5,8 +5,16 @@
 #include <fstream>
 #include <strstream>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
+struct vec3d;
+struct triangle;
+struct mesh;
+struct mat4x4;
+
+
+
 
 struct vec3d
 {
@@ -14,25 +22,40 @@ struct vec3d
     vec3d() : x(0), y(0), z(0) {}
     vec3d(int _x) : x(_x), y(_x), z(_x), w(1) {}
     vec3d(float _x) : x(_x), y(_x), z(_x), w(1) {}
-    vec3d(float _x, float _y, float _z) : x(_x), y(_y), z(_z), w(1) {}
+    vec3d(float _x, float _y, float _z) : x(_x), y(_y), z(_z), w(1) {cout<<_x<<" "<<_y<<" "<<_z<<endl;}
     vec3d(int _x, int _y, int _z) : x((float)_x), y((float)_y), z((float)_z), w(1) {}
 
+    vec3d operator + (const vec3d &rhs);
+    vec3d operator - (const vec3d &rhs);
+    vec3d operator * (const mat4x4 &rhs);
+
+    vec3d cross(const vec3d &rhs);
+    vec3d normal(void);
+    float dot(const vec3d &rhs);
+
+    friend ostream& operator<<(ostream& os, const vec3d& dt);
+
 };
+
+ostream& operator<<(ostream& os, const vec3d& dt)
+{
+    os << dt.x << " "<< dt.y << " "<< dt.z << endl;
+    return os;
+}
 
 struct triangle
 {
     vec3d p[3] = {0};
     triangle()
     {
-        cout<<"empty"<<endl;
         p[0] = vec3d();
         p[1] = vec3d();
         p[2] = vec3d();
-        int dp = 1;
+        int dp = 0;
     }
     triangle(vec3d _x, vec3d _y, vec3d _z)
     {
-        cout<<"with 3"<<endl;
+        //cout<<"with 3"<<endl;
         p[0] = vec3d(_x);
         p[1] = vec3d(_y);
         p[2] = vec3d(_z);
@@ -42,14 +65,21 @@ struct triangle
              float _x1, float _y1, float _z1, 
              float _x2, float _y2, float _z2)
     {
-        cout<<"longest"<<endl;
-        cout<<_x0<<_y0<<_z0<<endl;
+        //cout<<"long tri"<<endl;
         p[0] = vec3d(_x0, _y0, _z0);
         p[1] = vec3d(_x1, _y1, _z1);
         p[2] = vec3d(_x2, _y2, _z2);
         int dp = 1;
     }
+    friend ostream& operator<<(ostream& os, const triangle& dt);
 };
+ostream& operator<<(ostream& os, const triangle& dt)
+{
+    for (int i = 0; i<2; i++){
+        os << dt.p[i];
+    }
+    return os;
+}
 
 struct mesh
 {
@@ -59,12 +89,38 @@ struct mesh
 struct mat4x4
 {
     float m[4][4] = {0};
-
-        vec3d operator*(const vec3d &i)
-    {
-
-    }
 };
+
+vec3d vec3d::operator + (const vec3d &rhs) { return vec3d(this->x + rhs.x, this->y + rhs.y, this->z + rhs.z);}
+vec3d vec3d::operator - (const vec3d &rhs) { return vec3d(this->x - rhs.x, this->y - rhs.y, this->z - rhs.z);}
+vec3d vec3d::operator * (const mat4x4 &rhs) 
+{
+    vec3d out;
+
+    out.x = this->x * rhs.m[0][0] + this->y * rhs.m[1][0] + this->z * rhs.m[2][0] + this->w * rhs.m[3][0];
+    out.y = this->x * rhs.m[0][1] + this->y * rhs.m[1][1] + this->z * rhs.m[2][1] + this->w * rhs.m[3][1];
+    out.z = this->x * rhs.m[0][2] + this->y * rhs.m[1][2] + this->z * rhs.m[2][2] + this->w * rhs.m[3][2];
+    out.w = this->x * rhs.m[0][3] + this->y * rhs.m[1][3] + this->z * rhs.m[2][3] + this->w * rhs.m[3][3];
+
+    return out;
+}
+vec3d vec3d::cross(const vec3d &rhs)
+{
+    return vec3d(this->y * rhs.z - this->z * rhs.y,
+                 this->z * rhs.x - this->x * rhs.z,
+                 this->x * rhs.y - this->y * rhs.x);
+}
+
+vec3d vec3d::normal(void)
+{
+    float l = sqrtf(pow(this->x, 2) + pow(this->y, 2) + pow(this->z, 2));
+    return vec3d(this->x / l, this->y / l, this->z / l);
+}
+
+float vec3d::dot(const vec3d &rhs)
+{
+    return this->x * rhs.x + this->y * rhs.y + this->z * rhs.z;
+}
 
 class Example : public olc::PixelGameEngine
 {
@@ -133,70 +189,51 @@ private:
         matProj.m[3][3] = 0.0f;
         return matProj;
     }
-
-    void multiplyMatrixVector(vec3d &i, vec3d &o, mat4x4 &m)
+    vector<triangle> cubeMaker(vector<triangle> world, float Origo)
     {
-        o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
-        o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
-        o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
-        float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+        //world.push_back (triangle());
+        //{
+            //SOUTH
+        world.push_back(triangle(-0.5+Origo, -0.5+Origo, -0.5+Origo, -0.5+Origo, 0.5+Origo, -0.5+Origo, 0.5+Origo, 0.5+Origo, -0.5+Origo));
+        world.push_back(triangle(-0.5+Origo, -0.5+Origo, -0.5+Origo, 0.5+Origo, 0.5+Origo, -0.5+Origo, 0.5+Origo, -0.5+Origo, -0.5+Origo));
 
-        if (w != 0.0f)
-        {
-            o.x /= w;
-            o.y /= w;
-            o.z /= w;
-        }
-    }
+        //EAST
+        world.push_back(triangle(0.5+Origo, -0.5+Origo, -0.5+Origo, 0.5+Origo, 0.5+Origo, -0.5+Origo, 0.5+Origo, 0.5+Origo, 0.5+Origo));
+        world.push_back(triangle(0.5+Origo, -0.5+Origo, -0.5+Origo, 0.5+Origo, 0.5+Origo, 0.5+Origo, 0.5+Origo, -0.5+Origo, 0.5+Origo));
 
-    void normalizeVector(vec3d &input, vec3d &output)
-    {
-        float l = sqrtf(input.x * input.x + input.y * input.y + input.z * input.z);
-        output.x = input.x / l;
-        output.y = input.y / l;
-        output.z = input.z / l;
-    }
+        //NORTH
+        world.push_back(triangle(0.5+Origo, -0.5+Origo, 0.5+Origo, 0.5+Origo, 0.5+Origo, 0.5+Origo, -0.5+Origo, 0.5+Origo, 0.5+Origo));
+        world.push_back(triangle(0.5+Origo, -0.5+Origo, 0.5+Origo, -0.5+Origo, 0.5+Origo, 0.5+Origo, -0.5+Origo, -0.5+Origo, 0.5+Origo));
 
-    float dotProduct(vec3d &input1, vec3d &input2, vec3d &camera)
-    {
-        float D = (input1.x - camera.x) * input2.x +
-                  (input1.y - camera.y) * input2.y +
-                  (input1.z - camera.z) * input2.z;
-        return D;
+        //WEST
+        world.push_back(triangle(-0.5+Origo, -0.5+Origo, 0.5+Origo, -0.5+Origo, 0.5+Origo, 0.5+Origo, -0.5+Origo, 0.5+Origo, -0.5+Origo));
+        world.push_back(triangle(-0.5+Origo, -0.5+Origo, 0.5+Origo, -0.5+Origo, 0.5+Origo, -0.5+Origo, -0.5+Origo, -0.5+Origo, -0.5+Origo));
+
+        //TOP
+        world.push_back(triangle(-0.5+Origo, 0.5+Origo, -0.5+Origo, -0.5+Origo, 0.5+Origo, 0.5+Origo, 0.5+Origo, 0.5+Origo, 0.5+Origo));
+        world.push_back(triangle(-0.5+Origo, 0.5+Origo, -0.5+Origo, 0.5+Origo, 0.5+Origo, 0.5+Origo, 0.5+Origo, 0.5+Origo, -0.5+Origo));
+
+        //BOTTOM
+        world.push_back(triangle(-0.5+Origo, -0.5+Origo, 0.5+Origo, -0.5+Origo, -0.5+Origo, -0.5+Origo, 0.5+Origo, -0.5+Origo, -0.5+Origo));
+        world.push_back(triangle(-0.5+Origo, -0.5+Origo, 0.5+Origo, 0.5+Origo, -0.5+Origo, -0.5+Origo, 0.5+Origo, -0.5+Origo, 0.5+Origo));
+        //};
+        //cout << "Length of array = " << world.size() << std::endl;
+        return world;
     }
 
 public:
     bool OnUserCreate() override
     {
-
-        meshCube.tris = {
-            //SOUTH
-            {0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f},
-            {0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f},
-
-            //EAST
-            {1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f},
-            {1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f},
-
-            //NORTH
-            {1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f},
-            {1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f},
-
-            //WEST
-            {0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f},
-            {0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-
-            //TOP
-            {0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f},
-            {0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f},
-
-            //BOTTOM
-            {0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f},
-            {0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f},
-        };
-
+        //mesh meshCube;
+        meshCube.tris = cubeMaker(meshCube.tris, 0.0);
+        //cout << "Length of array = " << meshCube.tris.size() << std::endl;
+        /*for (auto tri : meshCube.tris)
+        {
+        //cout<<"meshCube.tris[0]"<<endl;
+        cout<<tri<<endl;
+        }*/
         matProj = matrixMakeProjection(fFovDeg, fAspectRatio, fFar, fNear);
-        cout<<"end"<<endl;
+        //cout<<"end"<<endl;
         return true;
     }
 
@@ -211,47 +248,42 @@ public:
         matRotX = matrixRotationX(fTheta * bias);
         matRotY = matrixRotationY(fTheta);
         matRotZ = matrixRotationZ(fTheta);
-
+        //cout<<"end2"<<endl;
+        //cout << "Length of array = " << meshCube.tris.size() << std::endl;
         for (auto tri : meshCube.tris)
         {
-            
+            //cout<<"end2"<<endl;
             for (int i = 0; i < 3; i++)
             {
-                multiplyMatrixVector(tri.p[i], triRotatedZ.p[i], matRotZ);
-                multiplyMatrixVector(triRotatedZ.p[i], triRotatedZX.p[i], matRotY);
+                //cout<<tri.p[i]<<" ";
+                triRotatedZ.p[i] = tri.p[i] * matRotZ;
+                triRotatedZX.p[i] = triRotatedZ.p[i] * matRotX;
 
                 triTranslated.p[i] = triRotatedZX.p[i];
-                triTranslated.p[i].z = triRotatedZX.p[i].z + 30.0f;
+                triTranslated.p[i].z = triRotatedZX.p[i].z + 15.0f;
             }
+            //cout<<"end"<<endl;
             vec3d normal, line1, line2;
-            line1.x = triTranslated.p[1].x - triTranslated.p[0].x;
-            line1.y = triTranslated.p[1].y - triTranslated.p[0].y;
-            line1.z = triTranslated.p[1].z - triTranslated.p[0].z;
 
-            line2.x = triTranslated.p[2].x - triTranslated.p[0].x;
-            line2.y = triTranslated.p[2].y - triTranslated.p[0].y;
-            line2.z = triTranslated.p[2].z - triTranslated.p[0].z;
+            line1 = triTranslated.p[1] - triTranslated.p[0];
+            line2 = triTranslated.p[2] - triTranslated.p[0];
 
-            normal.x = line1.y * line2.z - line1.z * line2.y;
-            normal.y = line1.z * line2.x - line1.x * line2.z;
-            normal.z = line1.x * line2.y - line1.y * line2.x;
+            normal = line1.cross(line2).normal();
 
-            normalizeVector(normal, normal);
+            triTranslated.p[0] = triTranslated.p[0] - vCamera;
+            float D = triTranslated.p[0].dot(normal);
 
-            float D = dotProduct(triTranslated.p[0], normal, vCamera);
             if (D < 0.0)
             {
                 vec3d lightDirection = {0.0f, 0.0f, -1.0f};
-                normalizeVector(lightDirection, lightDirection);
+                lightDirection = lightDirection.normal();
 
-                float dp = dotProduct(lightDirection, normal, vCamera) * 255;
-                cout<<dp<<" : ";
-                //dp = 1/dp;
-                cout<<dp<<endl;
+                lightDirection = lightDirection - vCamera;
+                float dp = lightDirection.dot(normal) * 255;
 
                 for (int i = 0; i < 3; i++)
                 {
-                    multiplyMatrixVector(triTranslated.p[i], triProjected.p[i], matProj);
+                    triProjected.p[i] = triTranslated.p[i] * matProj;
 
                     triProjected.p[i].x += 1.0f;
                     triProjected.p[i].y += 1.0f;
